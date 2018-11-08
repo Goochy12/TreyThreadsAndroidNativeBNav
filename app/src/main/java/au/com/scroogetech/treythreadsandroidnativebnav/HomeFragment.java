8337aca8678940eff80f09671ddecd71a69202b2
Liam <liamgooch@live.com.au>
2018-11-09 03:50:55 +1100

    added recycler view

--- app/src/main/java/au/com/scroogetech/treythreadsandroidnativebnav/HomeFragment.java
+++ app/src/main/java/au/com/scroogetech/treythreadsandroidnativebnav/HomeFragment.java
@@ -4,18 +4,45 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 
 public class HomeFragment extends Fragment {
 
+    private RecyclerView homeRecycler;
+    private RecyclerView.Adapter homeRecyclerAdapter;
+    private RecyclerView.LayoutManager homeRecyclerLayoutManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return  inflater.inflate(R.layout.fragment_home, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstaceState){
+        super.onViewCreated(view, savedInstaceState);
+
+        //get the recycler
+        homeRecycler = (RecyclerView) view.findViewById(R.id.homeRecyclerView);
+        //homeRecycler.setHasFixedSize(true);
+
+        //use a linear layout
+        homeRecyclerLayoutManager = new LinearLayoutManager(getActivity());
+        homeRecycler.setLayoutManager(homeRecyclerLayoutManager);
+
+        String[] data = {"1","2","3"};
+        //specify adapter
+        homeRecyclerAdapter = new homeRecAdpt(data);
+        homeRecycler.setAdapter(homeRecyclerAdapter);
     }
 
 }
---
+++ app/src/main/java/au/com/scroogetech/treythreadsandroidnativebnav/homeRecAdpt.java
@@ -0,0 +1,59 @@
+package au.com.scroogetech.treythreadsandroidnativebnav;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class homeRecAdpt extends RecyclerView.Adapter<homeRecAdpt.HomeViewHolder> {
+
+    private String[] homeData;
+
+    //constructor
+    public homeRecAdpt(String[] homeData){
+        this.homeData = homeData;
+    }
+
+    //create views
+    @NonNull
+    @Override
+    public HomeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        //create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.home_cards,parent,false);
+
+        HomeViewHolder hVH = new HomeViewHolder(v);
+
+        return hVH;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HomeViewHolder holder, int position){
+        holder.itemText.setText(homeData[position]);
+    }
+
+    @Override
+    public int getItemCount(){
+        return homeData.length;
+    }
+
+
+    //create the view holder
+    public static class HomeViewHolder extends RecyclerView.ViewHolder{
+        public View itemView;
+        public ImageView itemImage;
+        public TextView itemText;
+
+
+        public HomeViewHolder(View itemView){
+            super(itemView);
+            this.itemView = itemView;
+            itemText = (TextView) itemView.findViewById(R.id.homeCardText);
+            itemImage = (ImageView) itemView.findViewById(R.id.homeCardImage);
+        }
+    }
+
+
+}
