ca20fb8123c67b678f6aa20c38d6e8049fef0362
Liam <liamgooch@live.com.au>
2018-12-09 21:18:06 +1100

    added send email functionality

--- app/src/main/java/au/com/scroogetech/treythreadsandroidnativebnav/fragments/ContactFragment.java
+++ app/src/main/java/au/com/scroogetech/treythreadsandroidnativebnav/fragments/ContactFragment.java
@@ -1,21 +1,59 @@
 package au.com.scroogetech.treythreadsandroidnativebnav.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import au.com.scroogetech.treythreadsandroidnativebnav.R;
 
 
 public class ContactFragment extends Fragment {
 
+    private EditText name;
+    private EditText subject;
+    private EditText message;
+    private Button sendButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contact, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstaceState){
+        super.onViewCreated(view, savedInstaceState);
+
+        name = (EditText) view.findViewById(R.id.helpNameBox);
+        subject = (EditText) view.findViewById(R.id.helpSubjectBox);
+        message = (EditText) view.findViewById(R.id.helpMessageBox);
+        sendButton = (Button) view.findViewById(R.id.helpSendButton);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setType("message/rfc822");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"treythreads@gmail.com"});
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject.getText().toString());
+                emailIntent.putExtra(Intent.EXTRA_TEXT, message.getText().toString());
+
+                try {
+                    startActivity(Intent.createChooser(emailIntent,"Send Message"));
+                }catch (android.content.ActivityNotFoundException ex){
+                    Toast.makeText(getActivity(),"No email clients installed.",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
 }
