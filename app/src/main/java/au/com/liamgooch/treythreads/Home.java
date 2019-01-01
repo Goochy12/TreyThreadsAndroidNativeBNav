53e9c72f38373a0a3eb10a0ff1804da9721164c5
Liam <lgoo0003@student.monash.edu>
2019-01-02 01:04:26 +1100

    Revert "stuffed up"
    This reverts commit b9a58b4579335d898d2526605098a704b7612f7a.

--- app/src/main/java/au/com/liamgooch/treythreads/Home.java
+++ app/src/main/java/au/com/liamgooch/treythreads/Home.java
@@ -1,15 +1,8 @@
 package au.com.liamgooch.treythreads;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
-
-import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
-import com.aurelhubert.ahbottomnavigation.AHBottomNavigationAdapter;
-import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.appcompat.app.AppCompatActivity;
@@ -37,8 +30,7 @@ public class Home extends AppCompatActivity {
     private MenuItem accountItem;
     private MenuItem contactItem;
 
-//    public AHBottomNavigation navigation;
-    public AHBottomNavigation navigation;
+    public BottomNavigationView navigation;
     private Menu appBarMenu;
 
     //fragments
@@ -56,6 +48,86 @@ public class Home extends AppCompatActivity {
     private static final String TAG = "OHERE";
     private BraintreeFragment mBraintreeFragment;
     private static final String BRAINTREE_ACCESS_TOKEN = "access_token$sandbox$rrrnpdfp5mpm83zq$c4626caef58f28a29f18fdece41febc7";
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    //mTextMessage.setText(R.string.title_home);
+
+                    //set icon to clicked
+                    homeItem.setIcon(R.drawable.homeclicked);
+
+                    //set other icons to not clicked
+                    storeItem.setIcon(R.drawable.storedefault);
+                    cartItem.setIcon(R.drawable.cartdefault);
+                    accountItem.setIcon(R.drawable.user_default);
+                    contactItem.setIcon(R.drawable.contact_clicked);
+
+                    //load home fragment
+                    fragMan.beginTransaction().hide(active).show(homeFrag).commit();
+                    active = homeFrag;
+
+                    return true;
+                case R.id.navigation_store:
+                    //mTextMessage.setText("Store");
+
+                    //set icon to clicked
+                    storeItem.setIcon(R.drawable.storeclicked);
+
+                    //set other icons to not clicked
+                    homeItem.setIcon(R.drawable.homedefault);
+                    cartItem.setIcon(R.drawable.cartdefault);
+                    accountItem.setIcon(R.drawable.user_default);
+                    contactItem.setIcon(R.drawable.contact_clicked);
+
+                    //load store fragment
+                    fragMan.beginTransaction().hide(active).show(storeFrag).commit();
+                    active = storeFrag;
+
+                    return true;
+                case R.id.navigation_cart:
+                    //mTextMessage.setText("Cart");
+
+                    //set icon to clicked
+                    cartItem.setIcon(R.drawable.cartclicked);
+
+                    //set other icons to not clicked
+                    homeItem.setIcon(R.drawable.homedefault);
+                    storeItem.setIcon(R.drawable.storedefault);
+                    accountItem.setIcon(R.drawable.user_default);
+                    contactItem.setIcon(R.drawable.contact_clicked);
+
+                    //load cart fragment
+                    fragMan.beginTransaction().hide(active).show(cartFrag).commit();
+                    active = cartFrag;
+
+                    return true;
+                case R.id.navigation_account:
+                    //mTextMessage.setText("Contact");
+
+                    //set icon to clicked
+                    accountItem.setIcon(R.drawable.user_clicked);
+
+                    //set other icons to not clicked
+                    homeItem.setIcon(R.drawable.homedefault);
+                    storeItem.setIcon(R.drawable.storedefault);
+                    cartItem.setIcon(R.drawable.cartdefault);
+                    contactItem.setIcon(R.drawable.contact_clicked);
+
+                    //load contact fragment
+                    fragMan.beginTransaction().hide(active).show(accountFrag).commit();
+                    active = accountFrag;
+
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,52 +151,36 @@ public class Home extends AppCompatActivity {
 //        fragmentTransaction.add(R.id.fragmentLayout,homeFragment);
 //        fragmentTransaction.commit();
 
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        //set a null tint on the selected item
+        navigation.setItemIconTintList(null);
 
-//        AHBottomNavigation bottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation);
-//        AHBottomNavigationAdapter navigationAdapter = new AHBottomNavigationAdapter(this, R.menu.bottom_navigation_menu_3);
-//        navigationAdapter.setupWithBottomNavigation(bottomNavigation, tabColors);
+        //get the items
+        homeItem = navigation.getMenu().getItem(0);
+        storeItem = navigation.getMenu().getItem(1);
+        cartItem = navigation.getMenu().getItem(2);
+        accountItem = navigation.getMenu().getItem(3);
 
 
         //HomeFragment homeFragment = HomeFragment.newInstance("param1","param2");
 
 //        if (savedInstanceState == null) {
-        fragMan = getSupportFragmentManager();
-        fragMan.beginTransaction().add(R.id.fragmentLayout, homeFrag).commit();
-        active = homeFrag;
+            fragMan = getSupportFragmentManager();
+            fragMan.beginTransaction().add(R.id.fragmentLayout, homeFrag).commit();
+            active = homeFrag;
 
-        fragMan.beginTransaction().add(R.id.fragmentLayout, storeFrag).hide(storeFrag).commit();
-        fragMan.beginTransaction().add(R.id.fragmentLayout, cartFrag).hide(cartFrag).commit();
-        fragMan.beginTransaction().add(R.id.fragmentLayout, accountFrag).hide(accountFrag).commit();
-        fragMan.beginTransaction().add(R.id.fragmentLayout, contactFrag).hide(contactFrag).commit();
-        fragMan.beginTransaction().add(R.id.fragmentLayout, aboutFrag).hide(aboutFrag).commit();
+            fragMan.beginTransaction().add(R.id.fragmentLayout, storeFrag).hide(storeFrag).commit();
+            fragMan.beginTransaction().add(R.id.fragmentLayout, cartFrag).hide(cartFrag).commit();
+            fragMan.beginTransaction().add(R.id.fragmentLayout, accountFrag).hide(accountFrag).commit();
+            fragMan.beginTransaction().add(R.id.fragmentLayout, contactFrag).hide(contactFrag).commit();
+            fragMan.beginTransaction().add(R.id.fragmentLayout, aboutFrag).hide(aboutFrag).commit();
 
-        navigation = (AHBottomNavigation) findViewById(R.id.navigation);
-        AHBottomNavigationItem homeItem = new AHBottomNavigationItem(null,R.drawable.homeclicked);
-        AHBottomNavigationItem storeItem = new AHBottomNavigationItem(null, R.drawable.storedefault);
-        AHBottomNavigationItem cartItem = new AHBottomNavigationItem(null, R.drawable.cartdefault);
-        AHBottomNavigationItem userItem = new AHBottomNavigationItem(null, R.drawable.user_default);
 
-        navigation.addItem(homeItem);
-        navigation.addItem(storeItem);
-        navigation.addItem(cartItem);
-        navigation.addItem(userItem);
-
-        navigation.setAccentColor(Color.parseColor("#000000"));
-        navigation.setInactiveColor(Color.parseColor("#737373"));
-        navigation.setDefaultBackgroundColor(Color.parseColor("#F2F2F2"));
-        navigation.setBehaviorTranslationEnabled(false);
-        navigation.setCurrentItem(0);
+//        }else {
 //
-//        navigation.setColored(false);
-//        navigation.setColoredModeColors(Color.WHITE,ContextCompat.getColor(this, R.color.grey));
-//
-        navigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
-            @Override
-            public boolean onTabSelected(int position, boolean wasSelected) {
-                return itemSelcted(position);
-            }
-        });
+//        }
 
 
     }
@@ -177,48 +233,4 @@ public class Home extends AppCompatActivity {
         }
     }
 
-
-    public boolean itemSelcted(int position){
-        switch (position) {
-            case 0:
-                //mTextMessage.setText(R.string.title_home);
-
-
-                //load home fragment
-                fragMan.beginTransaction().hide(active).show(homeFrag).commit();
-                active = homeFrag;
-
-                return true;
-            case 1:
-                //mTextMessage.setText("Store");
-
-                //set icon to clicked
-
-                //load store fragment
-                fragMan.beginTransaction().hide(active).show(storeFrag).commit();
-                active = storeFrag;
-
-                return true;
-            case 2:
-                //mTextMessage.setText("Cart");
-
-                //set icon to clicked
-
-                //load cart fragment
-                fragMan.beginTransaction().hide(active).show(cartFrag).commit();
-                active = cartFrag;
-
-                return true;
-            case 3:
-                //mTextMessage.setText("Contact");
-
-
-                //load contact fragment
-                fragMan.beginTransaction().hide(active).show(accountFrag).commit();
-                active = accountFrag;
-
-                return true;
-        }
-        return false;
-    }
 }
